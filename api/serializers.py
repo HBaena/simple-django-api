3499b2f6bef182579b37a5b372f5f2fe8c527bac
HBaena <hbaena2adan@gmail.com>
2021-10-09 16:59:53 -0500

    Add feature: Adding validate status funtion to property model/serializer

--- api/serializers.py
+++ api/serializers.py
@@ -3,6 +3,8 @@ from .models import Property
 
 
 class PropertySerializer(serializers.HyperlinkedModelSerializer):
+    VALID_STATUS = {'Active', 'Inactive', 'Removed'}
+
     class Meta:
         model = Property
         fields = (
@@ -14,3 +16,8 @@ class PropertySerializer(serializers.HyperlinkedModelSerializer):
             'updated_at',
             'disabled_at',
             'status')
+
+    def validate_status(self, status):
+        if status not in self.VALID_STATUS:
+            raise serializers.ValidationError(f"Status need to be one of ({self.VALID_STATUS})")
+        return status
