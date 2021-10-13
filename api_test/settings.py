598d8b4356d1d7fdad12616a9b17294378ab1fb7
HBaena <hbaena2adan@gmail.com>
2021-10-13 02:40:26 -0500

    Updating files to fix allowed hosts

--- api_test/settings.py
+++ api_test/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = Path(Path.cwd() / "secret.key").read_text()
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = int(os.environ.get("DEBUG", default=0))
 
-ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="localhost").split(" ")
+ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS", default="*").split(" ")
 
 # Application definition
 
